7ff49ece7ccbbbfde9ca1772e830f47fe4678169
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-10-29 17:17:30 +0400

    add: 'Revising the Select Query I' problem

--- src/sql/basic_select/revising_the_select_query_1/Solution.sql
+++ src/sql/basic_select/revising_the_select_query_1/Solution.sql
@@ -1,4 +1,5 @@
--- Problem name: Day 9: Recursion 3
+-- Problem name: Revising the Select Query I
 -- Difficulty: Easy
 -- Problem link: https://www.hackerrank.com/challenges/30-recursion/problem
 
+select * from CITY where COUNTRYCODE = 'USA' and POPULATION > 100000;
