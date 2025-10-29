bc24984119f3d43243dab1fcf02bc32e54f5db2a
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-10-29 14:45:38 +0400

    add: 'Day 4: Class vs. Instance' problem

--- src/tutorials/thirty_days_of_code/day_4_class_vs_instance/Person.java
+++ src/tutorials/thirty_days_of_code/day_4_class_vs_instance/Person.java
@@ -4,9 +4,6 @@ package tutorials.thirty_days_of_code.day_4_class_vs_instance;
 // Difficulty: Easy
 // Problem link: https://www.hackerrank.com/challenges/30-class-vs-instance/problem
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Person {
@@ -14,15 +11,28 @@ public class Person {
 
     public Person(int initialAge) {
         // Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            age = 0;
+        } else {
+            age = initialAge;
+        }
     }
 
     public void amIOld() {
         // Write code determining if this person's age is old and print the correct statement:
-        System.out.println(/*Insert correct print statement here*/);
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
     }
 
     public void yearPasses() {
         // Increment this person's age.
+        age++;
     }
 
     public static void main(String[] args) {
