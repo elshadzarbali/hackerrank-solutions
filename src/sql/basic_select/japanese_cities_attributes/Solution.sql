5e7e570d0925ca901b52e3133ca1e2f4d371823c
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-10-30 11:40:36 +0400

    add: 'Japanese Cities' Attributes' problem

---
+++ src/sql/basic_select/japanese_cities_attributes/Solution.sql
@@ -0,0 +1,5 @@
+-- Problem name: Japanese Cities' Attributes
+-- Difficulty: Easy
+-- Problem link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+select * from city where countrycode = 'JPN';
