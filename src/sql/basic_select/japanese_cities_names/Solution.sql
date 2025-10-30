4303e9bef889921a307ae9e9c458cf14affc6494
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-10-30 11:42:29 +0400

    add: 'Japanese Cities' Names' problem

---
+++ src/sql/basic_select/japanese_cities_names/Solution.sql
@@ -0,0 +1,5 @@
+-- Problem name: Japanese Cities' Names
+-- Difficulty: Easy
+-- Problem link: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+select name from city where countrycode = 'JPN';
