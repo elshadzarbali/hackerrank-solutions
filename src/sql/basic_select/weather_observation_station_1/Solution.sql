c2c27796b739915b7c769e13d28e09723cf6ca6d
Elshad Zarbalizade <95376171+elshadzarbali@users.noreply.github.com>
2025-10-30 11:44:31 +0400

    add: 'Weather Observation Station 1' problem

---
+++ src/sql/basic_select/weather_observation_station_1/Solution.sql
@@ -0,0 +1,5 @@
+-- Problem name: Weather Observation Station 1
+-- Difficulty: Easy
+-- Problem link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+select city, state from station;
